6b345483de34ae1c331a9dcc666f66f20e6da76f
daniu <daniu@4677766e-dded-4b3a-9abc-a502f4ae862d>
2011-11-12 09:16:00 +0000

    automatic changes afaict

--- trunk/PackFileManager/PackFileManager/DBFileEditorControl.cs
+++ trunk/PackFileManager/PackFileManager/DBFileEditorControl.cs
@@ -303,50 +303,49 @@ namespace PackFileManager
 
         private void InitializeComponent()
         {
-            Settings settings1 = new Settings();
-            this.dataGridView = new DataGridView();
-            this.toolStrip = new ToolStrip();
-            this.addNewRowButton = new ToolStripButton();
-            this.cloneCurrentRow = new ToolStripButton();
-            this.copyToolStripButton = new ToolStripButton();
-            this.pasteToolStripButton = new ToolStripButton();
-            this.toolStripSeparator1 = new ToolStripSeparator();
-            this.exportButton = new ToolStripButton();
-            this.importButton = new ToolStripButton();
-            this.toolStripSeparator2 = new ToolStripSeparator();
-            this.useOnlineDefinitionsToolStripMenuItem = new ToolStripMenuItem();
-            this.openDBFileDialog = new OpenFileDialog();
-            this.unsupportedDBErrorTextBox = new TextBox();
-            this.checkBox1 = new CheckBox();
-            ((ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.addNewRowButton = new System.Windows.Forms.ToolStripButton();
+            this.cloneCurrentRow = new System.Windows.Forms.ToolStripButton();
+            this.copyToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.pasteToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exportButton = new System.Windows.Forms.ToolStripButton();
+            this.importButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.useOnlineDefinitionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openDBFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.unsupportedDBErrorTextBox = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.toolStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView
             // 
             this.dataGridView.AllowUserToAddRows = false;
-            this.dataGridView.Anchor = ((AnchorStyles)((((AnchorStyles.Top | AnchorStyles.Bottom) 
-            | AnchorStyles.Left) 
-            | AnchorStyles.Right)));
-            this.dataGridView.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
-            this.dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView.Location = new Point(0, 28);
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(0, 28);
             this.dataGridView.Name = "dataGridView";
             this.dataGridView.RowHeadersWidth = 100;
             this.dataGridView.ShowCellErrors = false;
             this.dataGridView.ShowEditingIcon = false;
             this.dataGridView.ShowRowErrors = false;
-            this.dataGridView.Size = new Size(876, 641);
+            this.dataGridView.Size = new System.Drawing.Size(876, 641);
             this.dataGridView.TabIndex = 1;
             this.dataGridView.VirtualMode = true;
-            this.dataGridView.CellPainting += new DataGridViewCellPaintingEventHandler(this.dataGridView_CellPainting);
-            this.dataGridView.DataBindingComplete += new DataGridViewBindingCompleteEventHandler(this.dataGridView_DataBindingComplete);
+            this.dataGridView.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.dataGridView_CellPainting);
+            this.dataGridView.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.dataGridView_DataBindingComplete);
             this.dataGridView.SelectionChanged += new System.EventHandler(this.dataGridView_SelectionChanged);
-            this.dataGridView.KeyPress += new KeyPressEventHandler(this.dataGridView_KeyPress);
+            this.dataGridView.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dataGridView_KeyPress);
             // 
             // toolStrip
             // 
-            this.toolStrip.Items.AddRange(new ToolStripItem[] {
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.addNewRowButton,
             this.cloneCurrentRow,
             this.copyToolStripButton,
@@ -355,52 +354,52 @@ namespace PackFileManager
             this.exportButton,
             this.importButton,
             this.toolStripSeparator2});
-            this.toolStrip.Location = new Point(0, 0);
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
             this.toolStrip.Name = "toolStrip";
-            this.toolStrip.Size = new Size(876, 25);
+            this.toolStrip.Size = new System.Drawing.Size(876, 25);
             this.toolStrip.TabIndex = 2;
             this.toolStrip.Text = "toolStrip";
             // 
             // addNewRowButton
             // 
-            this.addNewRowButton.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            this.addNewRowButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.addNewRowButton.Enabled = false;
-            this.addNewRowButton.ImageTransparentColor = Color.Magenta;
+            this.addNewRowButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.addNewRowButton.Name = "addNewRowButton";
-            this.addNewRowButton.Size = new Size(59, 22);
+            this.addNewRowButton.Size = new System.Drawing.Size(59, 22);
             this.addNewRowButton.Text = "Add Row";
             this.addNewRowButton.ToolTipText = "Add New Row";
             this.addNewRowButton.Click += new System.EventHandler(this.addNewRowButton_Click);
             // 
             // cloneCurrentRow
             // 
-            this.cloneCurrentRow.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            this.cloneCurrentRow.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.cloneCurrentRow.Enabled = false;
-            this.cloneCurrentRow.ImageTransparentColor = Color.Magenta;
+            this.cloneCurrentRow.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.cloneCurrentRow.Name = "cloneCurrentRow";
-            this.cloneCurrentRow.Size = new Size(68, 22);
+            this.cloneCurrentRow.Size = new System.Drawing.Size(68, 22);
             this.cloneCurrentRow.Text = "Clone Row";
             this.cloneCurrentRow.ToolTipText = "Clone Current Row";
             this.cloneCurrentRow.Click += new System.EventHandler(this.cloneCurrentRow_Click);
             // 
             // copyToolStripButton
             // 
-            this.copyToolStripButton.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            this.copyToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.copyToolStripButton.Enabled = false;
-            this.copyToolStripButton.ImageTransparentColor = Color.Magenta;
+            this.copyToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.copyToolStripButton.Name = "copyToolStripButton";
-            this.copyToolStripButton.Size = new Size(65, 22);
+            this.copyToolStripButton.Size = new System.Drawing.Size(65, 22);
             this.copyToolStripButton.Text = "&Copy Row";
             this.copyToolStripButton.ToolTipText = "Copy Current Row";
             this.copyToolStripButton.Click += new System.EventHandler(this.copyToolStripButton_Click);
             // 
             // pasteToolStripButton
             // 
-            this.pasteToolStripButton.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            this.pasteToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.pasteToolStripButton.Enabled = false;
-            this.pasteToolStripButton.ImageTransparentColor = Color.Magenta;
+            this.pasteToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.pasteToolStripButton.Name = "pasteToolStripButton";
-            this.pasteToolStripButton.Size = new Size(65, 22);
+            this.pasteToolStripButton.Size = new System.Drawing.Size(65, 22);
             this.pasteToolStripButton.Text = "&Paste Row";
             this.pasteToolStripButton.ToolTipText = "Paste Row from Clipboard";
             this.pasteToolStripButton.Click += new System.EventHandler(this.pasteToolStripButton_Click);
@@ -408,26 +407,26 @@ namespace PackFileManager
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new Size(6, 25);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
             // 
             // exportButton
             // 
-            this.exportButton.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            this.exportButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.exportButton.Enabled = false;
-            this.exportButton.ImageTransparentColor = Color.Magenta;
+            this.exportButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.exportButton.Name = "exportButton";
-            this.exportButton.Size = new Size(67, 22);
+            this.exportButton.Size = new System.Drawing.Size(67, 22);
             this.exportButton.Text = "Export TSV";
             this.exportButton.ToolTipText = "Export to tab-separated values";
             this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
             // 
             // importButton
             // 
-            this.importButton.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            this.importButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.importButton.Enabled = false;
-            this.importButton.ImageTransparentColor = Color.Magenta;
+            this.importButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.importButton.Name = "importButton";
-            this.importButton.Size = new Size(70, 22);
+            this.importButton.Size = new System.Drawing.Size(70, 22);
             this.importButton.Text = "Import TSV";
             this.importButton.ToolTipText = "Import from tab-separated values";
             this.importButton.Click += new System.EventHandler(this.importButton_Click);
@@ -435,19 +434,14 @@ namespace PackFileManager
             // toolStripSeparator2
             // 
             this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new Size(6, 25);
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
             // 
             // useOnlineDefinitionsToolStripMenuItem
             // 
-            settings1.SettingsKey = "";
-            settings1.TwcThreadId = "10595000";
-            settings1.UseFirstColumnAsRowHeader = false;
-            settings1.UseOnlineDefinitions = false;
-            this.useOnlineDefinitionsToolStripMenuItem.Checked = settings1.UseOnlineDefinitions;
             this.useOnlineDefinitionsToolStripMenuItem.CheckOnClick = true;
             this.useOnlineDefinitionsToolStripMenuItem.Enabled = false;
             this.useOnlineDefinitionsToolStripMenuItem.Name = "useOnlineDefinitionsToolStripMenuItem";
-            this.useOnlineDefinitionsToolStripMenuItem.Size = new Size(219, 22);
+            this.useOnlineDefinitionsToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
             this.useOnlineDefinitionsToolStripMenuItem.Text = "Use online definitions";
             this.useOnlineDefinitionsToolStripMenuItem.CheckedChanged += new System.EventHandler(this.useOnlineDefinitionsToolStripMenuItem_CheckedChanged);
             // 
@@ -457,24 +451,24 @@ namespace PackFileManager
             // 
             // unsupportedDBErrorTextBox
             // 
-            this.unsupportedDBErrorTextBox.Anchor = ((AnchorStyles)((((AnchorStyles.Top | AnchorStyles.Bottom) 
-            | AnchorStyles.Left) 
-            | AnchorStyles.Right)));
-            this.unsupportedDBErrorTextBox.Location = new Point(0, 28);
+            this.unsupportedDBErrorTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.unsupportedDBErrorTextBox.Location = new System.Drawing.Point(0, 28);
             this.unsupportedDBErrorTextBox.Multiline = true;
             this.unsupportedDBErrorTextBox.Name = "unsupportedDBErrorTextBox";
             this.unsupportedDBErrorTextBox.ReadOnly = true;
-            this.unsupportedDBErrorTextBox.ScrollBars = ScrollBars.Vertical;
-            this.unsupportedDBErrorTextBox.Size = new Size(876, 641);
+            this.unsupportedDBErrorTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.unsupportedDBErrorTextBox.Size = new System.Drawing.Size(876, 641);
             this.unsupportedDBErrorTextBox.TabIndex = 3;
             this.unsupportedDBErrorTextBox.Visible = false;
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new Point(422, 4);
+            this.checkBox1.Location = new System.Drawing.Point(422, 4);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new Size(183, 17);
+            this.checkBox1.Size = new System.Drawing.Size(183, 17);
             this.checkBox1.TabIndex = 4;
             this.checkBox1.Text = "Use First Column As Row Header";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -482,16 +476,16 @@ namespace PackFileManager
             // 
             // DBFileEditorControl
             // 
-            this.AutoScaleDimensions = new SizeF(6F, 13F);
-            this.AutoScaleMode = AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.toolStrip);
             this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.unsupportedDBErrorTextBox);
             this.Name = "DBFileEditorControl";
-            this.Size = new Size(876, 669);
+            this.Size = new System.Drawing.Size(876, 669);
             this.Enter += new System.EventHandler(this.DBFileEditorControl_Enter);
-            ((ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.toolStrip.ResumeLayout(false);
             this.toolStrip.PerformLayout();
             this.ResumeLayout(false);
