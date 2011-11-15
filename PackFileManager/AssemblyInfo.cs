56a3702d74f2041002a6b8a40bac40ba3ca8d903
daniu <daniu@4677766e-dded-4b3a-9abc-a502f4ae862d>
2011-11-15 20:11:48 +0000

    increased version number

--- PackFileManager/AssemblyInfo.cs
+++ PackFileManager/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿// Assembly PackFileManager, Version 1.7.0
 
-[assembly: System.Reflection.AssemblyVersion("1.7.0.0")]
+[assembly: System.Reflection.AssemblyVersion("1.7.4.0")]
 [assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
 [assembly: System.Reflection.AssemblyConfiguration("")]
 [assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
@@ -12,6 +12,6 @@
 [assembly: System.Reflection.AssemblyTrademark("")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Runtime.InteropServices.Guid("3f1cda43-b5a1-4c88-814b-4260dbe2fae6")]
-[assembly: System.Reflection.AssemblyFileVersion("1.7.3")]
+[assembly: System.Reflection.AssemblyFileVersion("1.7.4")]
 [assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.DisableOptimizations | System.Diagnostics.DebuggableAttribute.DebuggingModes.EnableEditAndContinue | System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | System.Diagnostics.DebuggableAttribute.DebuggingModes.Default)]
 
