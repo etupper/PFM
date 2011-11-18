3881960b22e1ff0657bf5a17f8b5bf4b421ffde2
iamdaniu <iamdaniu@4677766e-dded-4b3a-9abc-a502f4ae862d>
2011-11-18 09:33:54 +0000

    refactored readEntry for database files corrected type map parser increased version number using different formatting

--- Common/Common/DBTypeMap.cs
+++ Common/Common/DBTypeMap.cs
@@ -3,10 +3,8 @@ using System.Text.RegularExpressions;
 using System.IO;
 using System.Collections.Generic;
 
-namespace Common
-{
-	public class DBTypeMap
-	{
+namespace Common {
+    public class DBTypeMap {
         static Regex DB_FILE_TYPE_PATTERN = new Regex("DBFileTypes_([0-9]*).txt");
         public static readonly string DB_FILE_TYPE_DIR_NAME = "DBFileTypes";
         public static readonly string DB_TYPE_USER_DIR_NAME = "DBFileTypes_user";
@@ -15,67 +13,53 @@ namespace Common
 
         public static readonly DBTypeMap Instance = new DBTypeMap();
 
-        private DBTypeMap()
-        {
+        private DBTypeMap() {
             // prevent instantiation
         }
 
         public void initializeTypeMap(string basePath) {
             typeMap = loadDbFileTypes(basePath);
         }
-        public void loadFromXsd(string xsdFile)
-        {
+        public void loadFromXsd(string xsdFile) {
             typeMap = new XsdParser(xsdFile).loadXsd();
         }
         // returns true if this is the first time the user saves his files
-        public bool saveToFile(string directory)
-        {
+        public bool saveToFile(string directory) {
             // never overwrite downloaded files, use another subdirectory
             string dir = Path.Combine(directory, DB_TYPE_USER_DIR_NAME);
             bool result = false;
-            if (!Directory.Exists(dir))
-            {
+            if (!Directory.Exists(dir)) {
                 Directory.CreateDirectory(dir);
                 result = true;
             }
-            try
-            {
+            try {
                 Dictionary<int, List<TypeInfo>> versionToInfos = new Dictionary<int, List<TypeInfo>>();
 
                 // collect all typeinfo lists, sorted by version number
-                foreach (string key in typeMap.Keys)
-                {
+                foreach (string key in typeMap.Keys) {
                     List<TypeInfo> infos = typeMap[key];
-                    for (int i = 0; i < infos.Count; i++)
-                    {
-                        if (infos[i] != null)
-                        {
+                    for (int i = 0; i < infos.Count; i++) {
+                        if (infos[i] != null) {
                             List<TypeInfo> addTo = retrieveOrAdd<int>(versionToInfos, i);
                             addTo.Add(infos[i]);
                         }
                     }
                 }
                 Comparer<TypeInfo> comparer = new TypeInfoComparer();
-                foreach (int version in versionToInfos.Keys)
-                {
+                foreach (int version in versionToInfos.Keys) {
                     StreamWriter writer = new StreamWriter(Path.Combine(dir, "DBFileTypes_" + version + ".txt"));
                     List<TypeInfo> infos = versionToInfos[version];
                     infos.Sort(comparer);
                     bool nextFieldConditional = false;
-                    foreach (TypeInfo info in infos)
-                    {
+                    foreach (TypeInfo info in infos) {
                         // header: table name, tab, first encoded field
                         writer.Write(string.Format("{0}\t{1}", info.name, encodeField(info.fields[0])));
                         nextFieldConditional = info.fields[0].modifier == FieldInfo.Modifier.NextFieldIsConditional;
-                        for (int i = 1; i < info.fields.Count; i++)
-                        {
-                            if (nextFieldConditional)
-                            {
+                        for (int i = 1; i < info.fields.Count; i++) {
+                            if (nextFieldConditional) {
                                 // don't separate condition and condition target fields
                                 writer.Write(";");
-                            }
-                            else
-                            {
+                            } else {
                                 // semicolon at eol is marker that there are more fields to come
                                 writer.WriteLine(";");
                             }
@@ -87,9 +71,7 @@ namespace Common
                     }
                     writer.Close();
                 }
-            }
-            catch (Exception x)
-            {
+            } catch (Exception x) {
                 Directory.Move(dir, Path.Combine(directory, DB_TYPE_USER_DIR_NAME + ".save"));
                 throw x;
             }
@@ -98,7 +80,7 @@ namespace Common
         string encodeField(FieldInfo info) {
             string typeString = info.type == PackTypeCode.Empty ? info.Length.ToString() : info.type.ToString();
             string result = string.Format("{0},{1}", info.name, typeString);
-            switch(info.modifier) {
+            switch (info.modifier) {
                 case FieldInfo.Modifier.NextFieldRepeats:
                     result += ",*";
                     break;
@@ -108,46 +90,36 @@ namespace Common
             }
             return result;
         }
-        private static List<TypeInfo> retrieveOrAdd<T>(IDictionary<T, List<TypeInfo>> dict, T key)
-        {
+        private static List<TypeInfo> retrieveOrAdd<T>(IDictionary<T, List<TypeInfo>> dict, T key) {
             List<TypeInfo> list;
-            if (!dict.TryGetValue(key, out list))
-            {
+            if (!dict.TryGetValue(key, out list)) {
                 list = new List<TypeInfo>();
                 dict.Add(key, list);
             }
             return list;
         }
 
-        private static SortedDictionary<string, List<TypeInfo>> loadDbFileTypes(string basePath)
-        {
+        private static SortedDictionary<string, List<TypeInfo>> loadDbFileTypes(string basePath) {
             SortedDictionary<string, List<TypeInfo>> result = new SortedDictionary<string, List<TypeInfo>>();
             int headerVersion = 0;
 
             // prefer loading from user directory
             string path = Path.Combine(basePath, DB_TYPE_USER_DIR_NAME);
-            if (!Directory.Exists(path))
-            {
+            if (!Directory.Exists(path)) {
                 path = Path.Combine(basePath, DB_FILE_TYPE_DIR_NAME);
             }
 
             // find the highest header version
             string[] matchingFiles = Directory.GetFiles(path, "DBFileTypes_*");
-            foreach (string file in matchingFiles)
-            {
-                if (DB_FILE_TYPE_PATTERN.IsMatch(file))
-                {
+            foreach (string file in matchingFiles) {
+                if (DB_FILE_TYPE_PATTERN.IsMatch(file)) {
                     int version = -1;
-                    if (int.TryParse(DB_FILE_TYPE_PATTERN.Match(file).Groups[1].Value, out version))
-                    {
+                    if (int.TryParse(DB_FILE_TYPE_PATTERN.Match(file).Groups[1].Value, out version)) {
                         SortedDictionary<string, TypeInfo> fromFile = getTypeMapFromFile(file);
-                        foreach (string key in fromFile.Keys)
-                        {
+                        foreach (string key in fromFile.Keys) {
                             List<TypeInfo> addTo = retrieveOrAdd<string>(result, key);
-                            if (addTo.Count < version+1)
-                            {
-                                for (int i = addTo.Count; i < version+1; i++)
-                                {
+                            if (addTo.Count < version + 1) {
+                                for (int i = addTo.Count; i < version + 1; i++) {
                                     addTo.Add(null);
                                 }
                             }
@@ -160,89 +132,64 @@ namespace Common
             return result;
         }
 
-        public int HighestHeaderVersion
-        {
-            get
-            {
+        public int HighestHeaderVersion {
+            get {
                 return typeMap.Count;
             }
         }
 
-        public List<string> DBFileTypes
-        {
-            get
-            {
+        public List<string> DBFileTypes {
+            get {
                 List<string> result = new List<string>(typeMap.Keys);
                 return result;
             }
         }
 
-        public List<TypeInfo> this[string key]
-        {
-            get
-            {
+        public List<TypeInfo> this[string key] {
+            get {
                 List<TypeInfo> result = null;
                 typeMap.TryGetValue(key, out result);
                 return result;
             }
         }
 
-        private static SortedDictionary<string, TypeInfo> getTypeMapFromFile(string filepath)
-        {
+        private static SortedDictionary<string, TypeInfo> getTypeMapFromFile(string filepath) {
             string[] lines = File.ReadAllLines(filepath);
             return parseTypeMap(lines);
         }
 
-        private static SortedDictionary<string, TypeInfo> parseTypeMap(string[] lines)
-        {
+        private static SortedDictionary<string, TypeInfo> parseTypeMap(string[] lines) {
             SortedDictionary<string, TypeInfo> dictionary = new SortedDictionary<string, TypeInfo>();
-			TypeInfo lastInfo = null;
-            foreach (string str2 in lines)
-            {
-				if (str2.EndsWith(":")) {
-					lastInfo = new TypeInfo(str2.Replace(":", ""));
-					dictionary.Add(lastInfo.name, lastInfo);
-                // ignore empty and comment lines
-				} else if (!str2.StartsWith("#") && str2.Trim().Length != 0) {
-                    try
-                    {
-                        if (str2.Contains(";"))
-                        {
-                            foreach (string entry in str2.Split(";".ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
-                            {
-                                lastInfo.fields.Add(parseEntry(entry));
+            TypeInfo lastInfo = null;
+            foreach (string str2 in lines) {
+                if (str2.EndsWith(":")) {
+                    lastInfo = new TypeInfo(str2.Replace(":", ""));
+                    dictionary.Add(lastInfo.name, lastInfo);
+                    // ignore empty and comment lines
+                } else if (!str2.StartsWith("#") && str2.Trim().Length != 0) {
+                    string[] entries = str2.Split(";".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                    foreach (string entry in entries) {
+                        try {
+                            string[] split = entry.Split(',');
+                            FieldInfo info;
+                            if (split.Length == 3) {
+                                info = new FieldInfo(split[0], split[1].Replace(";", ""), split[2]);
+                            } else {
+                                info = new FieldInfo(split[0], split[1].Replace(";", ""));
                             }
-//                        FieldInfo info = (split.Length == 3)
-    					lastInfo.fields.Add(info);
+                            lastInfo.fields.Add(info);
+                        } catch (Exception) {
+                            throw;
                         }
-                        else
-                        {
-                            lastInfo.fields.Add(parseEntry(str2));
-                        }
-
-                    }
-                    catch (Exception)
-                    {
-                        string haha = str2;
-                        throw;
                     }
                 }
             }
             return dictionary;
         }
-        static FieldInfo parseEntry(string entry)
-        {
-            string[] split = entry.Split(',');
-            FieldInfo newInfo = split.Length == 3 ? new FieldInfo(split[0], split[1].Replace(";", ""), split[2])
-                : new FieldInfo(split[0], split[1].Replace(";", ""));
-            return newInfo;
-        }
-	}
+    }
 
-    class TypeInfoComparer : Comparer<TypeInfo>
-    {
-        public override int Compare(TypeInfo x, TypeInfo y)
-        {
+    class TypeInfoComparer : Comparer<TypeInfo> {
+        public override int Compare(TypeInfo x, TypeInfo y) {
             return x.name.CompareTo(y.name);
         }
     }
