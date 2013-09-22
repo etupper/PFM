8269d11ef433a6c7231052447748922b9324cf16
iamdaniu <iamdaniu@4677766e-dded-4b3a-9abc-a502f4ae862d>
2013-09-22 13:37:01 +0000

    Adding pack file browse dialog

---
+++ trunk/PackFileManager/PackBrowseDialog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PackFileManager {
+    public partial class PackBrowseDialog : Form {
+        public PackBrowseDialog() {
+            InitializeComponent();
+        }
+    }
+}
